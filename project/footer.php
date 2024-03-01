a73faa0e185dcf0a8afbb4f28a89d199f1384f68
znahamama <113527488+znahamama@users.noreply.github.com>
2024-03-02 01:05:18 +0200

    Add files via upload

---
+++ project/footer.php
@@ -0,0 +1,29 @@
+<section class="footer">
+<div class="box-container">
+<div class="box">
+<h3>Quick Links</h3>
+<a href="home.php">Home</a> 
+<a href="shop.php">Shop</a>
+<a href="contact.php">Contact</a>
+</div>
+<div class="box">
+<h3>Extra Links</h3>
+ <a href="login.php">Login</a>
+ <a href="register.php">Register</a> 
+ <a href="cart.php">Cart</a>
+  <a href="orders.php">orders</a>
+</div>
+<div class="box">
+<h3>Contact Info</h3>
+<p> <i class="fas fa-phone"></i> +201111785071 </p> <p> <i class="fas fa-phone"></i> +201226350856 </p>
+<p> <i class="fas fa-envelope"></i> psyycchhoo@gmail.com </p>
+<p> <i class="fas fa-map-marker-alt"></i> Egypt, Cairo - 11511 </p>
+</div>
+<div class="box">
+<h3>Follow Us</h3>
+<a href="#"> <i class="fab fa-facebook-f"></i> Facebook </a>
+  <a href="#"> <i class="fab fa-instagram"></i> Instagram </a> 
+</div>
+</div>
+<p class="credit"> Created By <span>Psycho</span> @<?php echo date('Y'); ?> | all rights reserved! </p>
+</section>
